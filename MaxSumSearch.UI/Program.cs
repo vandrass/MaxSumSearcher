c9e3f68861427046a641d04e22e6f4bd8e31ccfd
Ivan Goncharov <vandrass@gmail.com>
2022-01-09 20:33:13 +0200

    pathInsert was added

--- MaxSumSearch.UI/Program.cs
+++ MaxSumSearch.UI/Program.cs
@@ -1,4 +1,7 @@
-﻿namespace MaxSumSearch.UI
+﻿using System;
+using System.IO;
+
+namespace MaxSumSearch.UI
 {
     /// <summary>
     /// Start program class with console User Interface.
@@ -8,8 +11,42 @@
         /// <summary>
         /// Main UI Method.
         /// </summary>
-        public static void Main()
+        /// <param name="args">path to file.</param>
+        public static void Main(string[] args)
         {
+            string path;
+
+            do
+            {
+                path = PathInsert(ref args);
+                Console.WriteLine(File.Exists(path) ? "File exists" : "File doesn't exist!");
+            }
+            while (!File.Exists(path));
+
+            Console.ReadLine();
+        }
+
+        /// <summary>
+        /// Get Path from cmd arguments or from user.
+        /// </summary>
+        /// <param name="args">cmd arguments.</param>
+        /// <returns>path to file.</returns>
+        public static string PathInsert(ref string[] args)
+        {
+            string path;
+            if (args.Length > 0 && args[0] != null)
+            {
+                Console.WriteLine(" your path is {0}", args[0]);
+                path = args[0];
+                args[0] = null;
+            }
+            else
+            {
+                Console.Write("Enter Path to File: ");
+                path = Console.ReadLine();
+            }
+
+            return path;
         }
     }
 }
