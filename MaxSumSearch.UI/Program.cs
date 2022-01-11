fb71640fef7a3b3b8d1426e7a46f8dab1026c5f2
Ivan Goncharov <vandrass@gmail.com>
2022-01-11 18:53:04 +0200

    Program.cs was changed

--- MaxSumSearch.UI/Program.cs
+++ MaxSumSearch.UI/Program.cs
@@ -20,8 +20,16 @@ namespace MaxSumSearch.UI
 
             do
             {
-                path = PathInsert(ref args);
-                Console.WriteLine(File.Exists(path) ? "File exists" : "File doesn't exist!");
+                if (File.Exists(args[0]))
+                {
+                    path = args[0];
+                }
+                else
+                {
+                    Console.Write("Enter Path to File: ");
+                    path = Console.ReadLine();
+                    Console.WriteLine(File.Exists(path) ? "File exists" : "File doesn't exist!");
+                }
             }
             while (!File.Exists(path));
 
@@ -35,29 +43,6 @@ namespace MaxSumSearch.UI
             service.PrintMaxElementSumLine(content);
 
             Console.ReadLine();
-        }
-
-        /// <summary>
-        /// Get Path from cmd arguments or from user.
-        /// </summary>
-        /// <param name="args">cmd arguments.</param>
-        /// <returns>path to file.</returns>
-        public static string PathInsert(ref string[] args)
-        {
-            string path;
-            if (args.Length > 0 && args[0] != null)
-            {
-                Console.WriteLine(" your path is {0}", args[0]);
-                path = args[0];
-                args[0] = null;
-            }
-            else
-            {
-                Console.Write("Enter Path to File: ");
-                path = Console.ReadLine();
-            }
-
-            return path;
         }
     }
 }
