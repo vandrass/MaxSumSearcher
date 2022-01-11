f7ad7e8f9db1ca5f2b4f0029fd80902bd27a37fb
Ivan Goncharov <vandrass@gmail.com>
2022-01-11 19:32:40 +0200

    MaxSearch class was begining

--- MaxSumSearch.Application/MaxSumSearcher.cs
+++ MaxSumSearch.Application/MaxSumSearcher.cs
@@ -1,10 +1,16 @@
-﻿namespace MaxSumSearch.Application
+﻿using System;
+
+namespace MaxSumSearch.Application
 {
     /// <summary>
     /// Search and print a Line with max sum of elements.
     /// </summary>
     public class MaxSumSearcher : IMaxSumSearcher
     {
+        private long _maxSumOfLine;
+        private int _indexOfMaxSumLine;
+        private int[] _brokenLinesIndexes;
+
         /// <summary>
         /// Print number of line, with maximal sum of elements,
         /// and "broken" lines (with non numeric symbols) aside.
@@ -12,14 +18,29 @@
         /// <param name="content">object with file content.</param>
         public void PrintMaxElementSumLine(FileContent content)
         {
-            for (var i = 0; i < content.GetFileContent.GetUpperBound(0) + 1; i++)
+            var linesNumber = content.GetFileContent.GetUpperBound(0) + 1;
+            var brokenLinesCounter = 0;
+            var tmpSum = 0;
+            var parsed = true;
+
+            _brokenLinesIndexes = new int[linesNumber];
+
+            for (var i = 0; i < linesNumber; i++)
             {
-                foreach (var cont in content.GetFileContent[i])
+                for (var j = 0; j < content.GetFileContent[i].Length && parsed == true; j++)
                 {
-                    System.Console.Write(cont + "\t");
+                    if (parsed = long.TryParse(content.GetFileContent[i][j], out long number))
+                    {
+
+                    }
+                    else
+                    {
+                        _brokenLinesIndexes[brokenLinesCounter] = i;
+                        brokenLinesCounter++;
+                    }
                 }
 
-                System.Console.WriteLine();
+                Console.WriteLine();
             }
         }
     }
