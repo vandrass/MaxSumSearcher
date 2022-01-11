79f26f61fcecf0a437ccb00f9c1a02fabeda700c
Ivan Goncharov <vandrass@gmail.com>
2022-01-11 21:10:01 +0200

    MaxSumSearcher class complited

--- MaxSumSearch.Application/MaxSumSearcher.cs
+++ MaxSumSearch.Application/MaxSumSearcher.cs
@@ -20,31 +20,57 @@ namespace MaxSumSearch.Application
         /// <param name="content">object with file content.</param>
         public void PrintMaxElementSumLine(FileContent content)
         {
-            var linesNumber = content.GetFileContent.GetUpperBound(0) + 1;
-            var brokenLinesCounter = 0;
-            var tmpSum = 0;
-            var parsed = true;
-
+            int linesNumber = content.GetFileContent.GetUpperBound(0) + 1;
+            int brokenLinesCounter = 0;
+            double tmpSum;
+            bool parsed;
             _brokenLinesIndexes = new int[linesNumber];
 
             for (var i = 0; i < linesNumber; i++)
             {
                 parsed = true;
+                tmpSum = 0;
 
                 for (var j = 0; j < content.GetFileContent[i].Length && parsed == true; j++)
                 {
                     if (parsed = double.TryParse(content.GetFileContent[i][j], NumberStyles.AllowDecimalPoint, provider, out double number))
                     {
-
+                        tmpSum += number;
                     }
                     else
                     {
-                        _brokenLinesIndexes[brokenLinesCounter] = i;
+                        _brokenLinesIndexes[brokenLinesCounter] = i + 1;
                         brokenLinesCounter++;
                     }
                 }
 
-                Console.WriteLine();
+                if (tmpSum > _maxSumOfLine)
+                {
+                    _maxSumOfLine = tmpSum;
+                    _indexOfMaxSumLine = i + 1;
+                }
+            }
+
+            PrintMaxLineAndBrokenLines();
+        }
+
+        private void PrintMaxLineAndBrokenLines()
+        {
+            Console.WriteLine("Line with maximal Sum of numbers is: " + _indexOfMaxSumLine);
+
+            Console.Write("Broken Lines Numbers: ");
+
+            for (int i = 0; i < _brokenLinesIndexes.Length; i++)
+            {
+                if (_brokenLinesIndexes[i] != 0)
+                {
+                    Console.Write(_brokenLinesIndexes[i]);
+                }
+
+                if ( i + 1 != _brokenLinesIndexes.Length && _brokenLinesIndexes[i + 1] != 0)
+                {
+                    Console.Write(", ");
+                }
             }
         }
     }
